bfd3a428a50f78be8922c3e40e5e8db0005484b8
ishak_sebsib <imu.isomusi@gmail.com>
2022-11-30 01:25:45 +0300

    stream audio from url updated

--- src/component/musicbar.componet.jsx
+++ src/component/musicbar.componet.jsx
@@ -1,4 +1,4 @@
-import music from "../songs/2002.mp3";
+import music from "https://jesusful.com/wp-content/uploads/music/2022/08/Anne_Marie_-_2002_(Jesusful.com).mp3";
 import "./musicbar.componet.css";
 import AudioPlayer from "react-h5-audio-player";
 
